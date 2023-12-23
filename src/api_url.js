b76bdb1d114514a673fba7562a3b89c2ee14b62f
Raman Madhukar <146058279+007raman@users.noreply.github.com>
2023-12-23 19:13:12 +0530

    apiurl changed

--- src/api_url.js
+++ src/api_url.js
@@ -1,6 +1,6 @@
 // const BASE_URL = "https://whale-app-rqcs6.ondigitalocean.app/api";
 // const BASE_URL = 'https://lobster-app-5gysu.ondigitalocean.app/api'
-const BASE_URL = 'https://btc-backend-3fxj.onrender.com/api'
+const BASE_URL = 'https://whale-app-lloim.ondigitalocean.app/api'
 // const BASE_URL = 'http://localhost:8000/api'
 export const redirectUrl = 'https://btc-frontend.vercel.app'
 // export const redirectUrl = 'http://localhost:3002'

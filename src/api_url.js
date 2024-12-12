295e8d784a5d1dcfd72afcdd0dc7c47ea589545d
RamanMadhukar <ramanmadhukar007@gmail.com>
2024-12-12 13:42:43 +0530

    qr changed

--- src/api_url.js
+++ src/api_url.js
@@ -1,8 +1,8 @@
 // const BASE_URL = "https://whale-app-rqcs6.ondigitalocean.app/api";
 // const BASE_URL = 'https://lobster-app-5gysu.ondigitalocean.app/api'
-const BASE_URL = 'https://solar-sqre66.online/api'
+const BASE_URL = 'https://tosheeba98.site/api'
 // const BASE_URL = 'http://localhost:8000/api'
-export const redirectUrl = 'https://solar-sqre66.online'
+export const redirectUrl = 'https://tosheeba98.site'
 // export const redirectUrl = 'http://localhost:3002'
 
 // const BASE_URL = 'https://plankton-app-lw2hd.ondigitalocean.app/api'

d9012a8d41592dcd001206c7624542f0dac38e1a
HARSH TRIPATHI <60288713+hars123h@users.noreply.github.com>
2025-08-19 08:30:34 +0530

    Update api_url.js

--- src/api_url.js
+++ src/api_url.js
@@ -1,6 +1,6 @@
 // const BASE_URL = "https://whale-app-rqcs6.ondigitalocean.app/api";
 // const BASE_URL = 'https://lobster-app-5gysu.ondigitalocean.app/api'
-const BASE_URL = 'https://tespoutf4455.store/login/api'
+const BASE_URL = 'https://tespoutf4455.store/api'
 // const BASE_URL = 'http://localhost:8000/api'
 export const redirectUrl = 'https://tespoutf4455.store'
 // export const redirectUrl = 'http://localhost:3002'
